e3c17bdb607b23c7531b8e332c667a9804fee4e9
Josh Manders <josh@joshmanders.com>
2024-10-19 15:29:28 -0500

    remove unnecessary service provider

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,12 +3,11 @@
 namespace Tests;
 
 use Orchestra\Testbench;
-use NiftyCo\Attachments\ServiceProvider;
 
 abstract class TestCase extends Testbench\TestCase
 {
     protected function getPackageProviders($app)
     {
-        return [Fixtures\ServiceProvider::class, ServiceProvider::class];
+        return [Fixtures\ServiceProvider::class];
     }
 }
